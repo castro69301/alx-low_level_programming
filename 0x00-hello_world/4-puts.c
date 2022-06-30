2bdf7f5fec220a55796cc7234bc632cae72a472b
castro69301 <mwenyidavidcastro@gmail.com>
2022-06-30 16:44:58 -0700

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
+ * */
+in main(void)
+{
+puts("\"Programming is like building a multiligual puzzle");
+return (0);
+}
