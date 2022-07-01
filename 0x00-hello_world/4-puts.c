4379bb169119125919e72933ae836d0f90c0fbe6
castro69301 <mwenyidavidcastro@gmail.com>
2022-06-30 18:24:45 -0700

    PUt

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
-#!/bin/bash
-1  #include <stdio.h>
-2  /**
-3   *Description: main - Prints out a string to stdout.
-4   *Return: 0 if success
-5   */
-6   inti main(void)
-7   {
-8   puts("\"Programming is like building a multilingual puzzle")
-9   return (0) 
-10  }
+#include <stdio.h>
+
+#include <string.h>
+
+
+int main(void)   {
+	//initializing strings
+	char str1 
+		[] = "Programming is like building a multilingual puzzl"
+	writing  to stdout
+puts (str1);
+	return 0: 
+
+	}
