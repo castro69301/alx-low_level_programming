7fdcd2276d71edd5b2ad8ab05db7845419c62c65
castro69301 <mwenyidavidcastro@gmail.com>
2022-06-30 17:38:30 -0700

    Helloputs

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
-#!/bin/bash
 #include <stdio.h>
-/**
- *  * Description: main - Prints out a string to stdout.
- *   * Return: 0 if success.
- *    */
-int main(void)
-{
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
-}
+2   /**
+3   * Description: main - Prints out a string to stdout.
+4   * Return: 0 if success.
+5    */
+6    int main(void)
+7   {
+8	printf("with proper grammar, but the outcome is a piece of art,\n");
+9	return (0);
+10  }
+
