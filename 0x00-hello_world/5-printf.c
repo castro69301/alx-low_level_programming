54d6e05236cdfa18fc3b9c404314674a937948d8
castro69301 <mwenyidavidcastro@gmail.com>
2022-06-30 17:24:29 -0700

    Helloputs

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+1 #include <stdio.h>
+2 /**
+3 *Description: main - Prints out a string to ask
+4 *Return: 0 if success.
+5 */
+6 int main(void) 
+7 {
+8 printf("with proper grammer , but the outcome is a piece of art,\n")
+9 return (0);
+10 }
+
