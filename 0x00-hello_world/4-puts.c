6c8846e0324dbd62676fd09b43d20673b8b3758b
castro69301 <mwenyidavidcastro@gmail.com>
2022-06-30 17:04:42 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #!/bin/bash
-#include <stdio.h>
-/**
- * Description: main - Prints out a string to stdout.
- * Return: 0 if success.
- * */
-in main(void)
-{
-puts("\"Programming is like building a multiligual puzzle");
-return (0);
-}
+1  #include <stdio.h>
+2  /**
+3   *Description: main - Prints out a string to stdout.
+4   *Return: 0 if success
+5   */
+6   inti main(void)
+7   {
+8   puts("\"Programming is like building a multilingual puzzle")
+9   return (0) 
+10  }
