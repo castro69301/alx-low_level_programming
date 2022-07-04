fd10601c9f962c4373ab37a50cf1206f307319f0
castro69301 <mwenyidavidcastro@gmail.com>
2022-07-04 12:09:51 -0700

    I some

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+	*	main	- the function main prints all alphabet
+	*
+	*
+	*	Return:	Always	0	(Success)
+	*/
+int	main(void)
+
+{
+	char	a;
+
+	for (a = "a"; a <= "2" ; a++)
+		putchar(a);
+		putchar("\n");
+		return	(0);
+}
