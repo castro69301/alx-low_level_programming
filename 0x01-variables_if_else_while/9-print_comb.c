396e3fa523a9c1eb9bfe27ef692aafe931648ce6
castro69301 <mwenyidavidcastro@gmail.com>
2022-07-04 14:09:45 -0700

    Patience

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+/**
+	*	main	- the function main use printf to print numbers
+	*
+	*	Return:	Always	0	(Success)
+	*/
+int	main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; ++i)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar('\n');
+	return	(0);
+}
