b8c6f702d1f549d7d7c025ba736a6b7478349f6b
castro69301 <mwenyidavidcastro@gmail.com>
2022-07-05 15:44:33 -0700

    Sometimes

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+	*	print_alphabet -> prints the alphab
+	*/
+void	print_alphabet(void)
+{
+	int j;
+
+	for (j = 'a'; j <= 'z'; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+}
