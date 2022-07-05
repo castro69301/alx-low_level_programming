081037a4f33ddd7be29e427f3b2fba7e93a19f7a
castro69301 <mwenyidavidcastro@gmail.com>
2022-07-05 14:47:11 -0700

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,20 @@
 #include<main.h>
+
 /**
-	*	main	- prints the string "Main" from a character array
-	*	Return:	Always	0	(success)
+	*	main	- prints putchar
+	*	Returns:	Always	0	(Success)
 	*/
 int	main(void)
 {
-	char c[] = "Main";
-	int i;
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-	for (i = 0; i < 9; i++)
-	{
-		_putchar(c[i]);
-	}
-	_putchar("\n");
 	return	(0);
 }
